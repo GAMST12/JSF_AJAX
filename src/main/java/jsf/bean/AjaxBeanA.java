8e1a03ac753a3ccbeceadc3d852f1a3e453f6ca6
Anton Bessmeltsev <gamst9@gmail.com>
2016-12-06 14:38:12 +0200

    Initial commit

---
+++ src/main/java/jsf/bean/AjaxBeanA.java
@@ -0,0 +1,22 @@
+package jsf.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class AjaxBeanA {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getProcessMessage() {
+        return "Hello " + message;
+    }
+}
